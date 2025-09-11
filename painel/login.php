ee26be4c7b0db2a495e3927bf7b4d4fe8ae68144
OBATA TECH SOLUCOES <obatatech@gmail.com>
2025-09-11 15:20:57 -0300

    Use PDO for panel login

--- painel/login.php
+++ painel/login.php
@@ -1,16 +1,18 @@
 <?php
     include("includes/connect.php");
 
-    $admin_agentCode = pg_escape_string($conn, $_POST['agentCode']);
-    $admin_senha = pg_escape_string($conn, $_POST['senha']); 
+    $admin_agentCode = $_POST['agentCode'] ?? '';
+    $admin_senha = $_POST['senha'] ?? '';
     $auth = 'admin_in';
 
-    $query = pg_query($conn, "SELECT * FROM agents WHERE agentCode = '$admin_agentCode' AND senha = '$admin_senha'");
-    if (pg_num_rows($query) == 0) {
+    $stmt = $conn->prepare('SELECT * FROM agents WHERE agentCode = :agentCode AND senha = :senha');
+    $stmt->execute([':agentCode' => $admin_agentCode, ':senha' => $admin_senha]);
+
+    if ($stmt->rowCount() === 0) {
         http_response_code(400);
         echo json_encode(['message' => 'Usúario/senha incorretos']);
     } else {
-        $row = pg_fetch_array($query);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
         setcookie("admin_id", $row["id"]);
         setcookie("admin_pass", $admin_senha);
         setcookie("auth", $auth);
